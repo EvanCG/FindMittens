267449c458f486a5991d04001980bc897568f94f
Evan Griffith <evan@evangriffith.net>
2024-02-14 16:40:17 -0800

    Cat is now moving around the page randomly

--- scripts/content.js
+++ scripts/content.js
@@ -1,18 +1,22 @@
 
-  /**
-   * Regular expression to find all "words" in a string.
-   *
-   * Here, a "word" is a sequence of one or more non-whitespace characters in a row. We don't use the
-   * regular expression character class "\w" to match against "word characters" because it only
-   * matches against the Latin alphabet. Instead, we match against any sequence of characters that
-   * *are not* a whitespace characters. See the below link for more information.
-   *
-   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
-   */
+  const body = document.querySelector('body');
   const catImage = document.createElement('img');
   // Use the same styling as the publish information in an article's header
-  catImage.setAttribute('src', 'https://i.pinimg.com/564x/87/dc/34/87dc3498f492d74f452a96d6091f24a5.jpg');
-  
-  const body = document.querySelector('body');
+  catImage.setAttribute('src', 'https://i.imgur.com/Ia9iet9.png');
+
+
+  let widthMax = body.offsetWidth - 20; 
+  let heightMax = body.offsetHeight - 20;
+
+  let randomWidth = Math.floor(Math.random() * widthMax);
+  let randomHeight = Math.floor(Math.random() * heightMax);
+
+  catImage.style.width = '20px';
+  catImage.style.position = 'absolute';
+  catImage.style.left = `${randomWidth}px`;
+  catImage.style.top = `${randomHeight}px`;
+  catImage.setAttribute('alt', `${randomWidth} and ${randomHeight}`);
+  catImage.style.zIndex = '9000'
+
   body.appendChild(catImage);
 
